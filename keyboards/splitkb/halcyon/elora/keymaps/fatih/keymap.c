507175dfa69da6b2459a7c347c332d2bf6eb9740
Fatih Arslan <ftharsln@gmail.com>
2025-06-27 15:11:02 +0300

    Use correct keyword
    Signed-off-by: Fatih Arslan <ftharsln@gmail.com>

--- keyboards/splitkb/halcyon/elora/keymaps/fatih/keymap.c
+++ keyboards/splitkb/halcyon/elora/keymaps/fatih/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_QWERTY] = LAYOUT_elora_hlc(
      KC_EQL  ,       KC_1 ,  KC_2   ,  KC_3  ,   KC_4 ,            KC_5 ,                                         KC_6 ,  KC_7 ,  KC_8 ,   KC_9 ,   KC_0 ,   KC_MINS,
-     KC_TAB  ,       KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,            KC_T ,                                         KC_Y,   KC_U ,  KC_I ,   KC_O ,   KC_P ,   KC_BSLC,
+     KC_TAB  ,       KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,            KC_T ,                                         KC_Y,   KC_U ,  KC_I ,   KC_O ,   KC_P ,   KC_BSLS,
      KC_LCTL ,       KC_A ,  KC_S   ,  KC_D  ,   LT(_NUMPAD,KC_F), LSFT_T(KC_G),                                  RSFT_T(KC_H),   KC_J ,  KC_K ,   KC_L ,   KC_SCLN, CTL_QUOT,
      OSM(MOD_LSFT) , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,            KC_B , KC_LALT,MACLOCK,     KC_ESC  , KC_RALT, KC_N,   KC_M ,  KC_COMM, KC_DOT,  KC_SLSH, OSM(MOD_RSFT),
                                 KC_LEFT , KC_RIGHT, KC_LGUI, KC_BSPC , HYPR(KC_5),             HYPR(KC_8), KC_ENT ,LT(_SYMBOLS, KC_SPC), KC_UP, KC_DOWN,
