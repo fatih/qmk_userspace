5f0dc3c194497c4ba3fd4b5c77ebfc9ea16fd3fd
Fatih Arslan <ftharsln@gmail.com>
2025-07-20 12:29:10 +0300

    Change Shift keys, let's try out these
    Signed-off-by: Fatih Arslan <ftharsln@gmail.com>

--- keyboards/splitkb/halcyon/elora/keymaps/fatih/keymap.c
+++ keyboards/splitkb/halcyon/elora/keymaps/fatih/keymap.c
@@ -37,9 +37,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
  * |  Tab   |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |        |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |  Ctrl  |   A  |   S  |   D  |   F  |SHF(G)|                              |SHF(H)|   J  |   K  |   L  | ;  : |Ctrl/' "|
+ * |  Ctrl  |   A  |   S  |   D  |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |Ctrl/' "|
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  | Alt  | Lock |  | Esc  |  Alt |   N  |   M  | ,  < | . >  | /  ? | RShift |
+ * | LAlt   |   Z  |   X  |   C  |   V  |   B  | Shift| Lock |  | Esc  | Shift|   N  |   M  | ,  < | . >  | /  ? | RAlt   |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
  *                        | Left | Right|  GUI | Bpsc | Hypr |  | Hypr | Enter| Space| Up   | Down |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
@@ -51,8 +51,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_elora_hlc(
      KC_EQL  ,       KC_1 ,  KC_2   ,  KC_3  ,   KC_4 ,            KC_5 ,                                         KC_6 ,  KC_7 ,  KC_8 ,   KC_9 ,   KC_0 ,   KC_MINS,
      KC_TAB  ,       KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,            KC_T ,                                         KC_Y,   KC_U ,  KC_I ,   KC_O ,   KC_P ,   KC_BSLS,
-     KC_LCTL ,       KC_A ,  KC_S   ,  KC_D  ,   LT(_NUMPAD,KC_F), LSFT_T(KC_G),                                  RSFT_T(KC_H),   KC_J ,  KC_K ,   KC_L ,   KC_SCLN, CTL_QUOT,
-     OSM(MOD_LSFT) , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,            KC_B , KC_LALT,MACLOCK,     KC_ESC  , KC_RALT, KC_N,   KC_M ,  KC_COMM, KC_DOT,  KC_SLSH, OSM(MOD_RSFT),
+     KC_LCTL ,       KC_A ,  KC_S   ,  KC_D  ,   LT(_NUMPAD,KC_F), KC_G,                                          KC_H,   KC_J ,  KC_K ,   KC_L ,   KC_SCLN, CTL_QUOT,
+     KC_LALT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,            KC_B , OSM(MOD_LSFT), MACLOCK,     KC_ESC  , OSM(MOD_RSFT), KC_N,   KC_M ,  KC_COMM, KC_DOT,  KC_SLSH, KC_RALT,
                                 KC_LEFT , KC_RIGHT, KC_LGUI, KC_BSPC , HYPR(KC_5),             HYPR(KC_8), KC_ENT ,LT(_SYMBOLS, KC_SPC), KC_UP, KC_DOWN,
      KC_MUTE, KC_NO,  KC_NO, KC_NO, KC_NO,                                                                KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO
     ),
