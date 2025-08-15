f416c96309bbc30e9f02ca448a1a85e88b8620c5
Fatih Arslan <ftharsln@gmail.com>
2025-08-15 08:54:07 +0300

    Let's try to diable backlights
    Signed-off-by: Fatih Arslan <ftharsln@gmail.com>

--- keyboards/splitkb/halcyon/elora/keymaps/fatih/keymap.c
+++ keyboards/splitkb/halcyon/elora/keymaps/fatih/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * | LAlt   |   Z  |   X  |   C  |   V  |   B  | Shift| Lock |  | Esc  | Shift|   N  |   M  | ,  < | . >  | /  ? | RAlt   |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        | Left | Right|      | GUI  | Bpsc |  | Enter| Space|      | Up   | Down |
+ *                        | Left | Right| Shift| GUI  | Bpsc |  | Enter| Space|Shift | Up   | Down |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  * ,-----------------------------------.                                              ,-----------------------------------.
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB  ,       KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,            KC_T ,                                         KC_Y,   KC_U ,  KC_I ,   KC_O ,   KC_P ,   KC_BSLS,
      KC_LCTL ,       KC_A ,  KC_S   ,  KC_D  ,   LT(_NUMPAD,KC_F), KC_G,                                          KC_H,   KC_J ,  KC_K ,   KC_L ,   KC_SCLN, CTL_QUOT,
      OSM(MOD_LALT) , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,            KC_B , OSM(MOD_LSFT), HYPR(KC_5),     KC_ESC  , OSM(MOD_RSFT), KC_N,   KC_M ,  KC_COMM, KC_DOT,  KC_SLSH, OSM(MOD_RALT),
-                                KC_LEFT , KC_RIGHT, KC_NO, KC_LGUI, KC_BSPC,                    KC_ENT ,LT(_SYMBOLS, KC_SPC), KC_NO, KC_UP, KC_DOWN,
+                                KC_LEFT , KC_RIGHT, OSM(MOD_LSFT), KC_LGUI, KC_BSPC,                    KC_ENT ,LT(_SYMBOLS, KC_SPC), OSM(MOD_RSFT), KC_UP, KC_DOWN,
      KC_MUTE, KC_NO,  KC_NO, KC_NO, KC_NO,                                                                KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO
     ),
 
--- keyboards/splitkb/halcyon/elora/keymaps/fatih/rules.mk
+++ keyboards/splitkb/halcyon/elora/keymaps/fatih/rules.mk
@@ -1,5 +1,7 @@
 ENCODER_MAP_ENABLE = yes
 RGBLIGHT_ENABLE = no
+BACKLIGHT_ENABLE = no
 
 # This adds module functionality to your keyboard (files found in users/halcyon_modules)
 USER_NAME := halcyon_modules
+
