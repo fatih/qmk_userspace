f7a6bec1ea4b0bafc7400812ff6bb56143c95341
Fatih Arslan <ftharsln@gmail.com>
2025-11-03 12:02:33 +0300

    Fix
    Signed-off-by: Fatih Arslan <ftharsln@gmail.com>

--- keyboards/splitkb/halcyon/elora/keymaps/fatih/keymap.c
+++ keyboards/splitkb/halcyon/elora/keymaps/fatih/keymap.c
@@ -49,12 +49,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------'                                              `-----------------------------------'
  */
     [_QWERTY] = LAYOUT_elora_hlc(
-     KC_GRAVE,    KC_1 ,  KC_2 ,  KC_3  ,   KC_4 ,            KC_5 ,                                         KC_6 ,  KC_7 ,  KC_8 ,   KC_9 ,   KC_0 ,   KC_MINS,
-     KC_TAB  ,    KC_Q ,  KC_W ,  KC_E  ,   KC_R ,            KC_T ,                                         KC_Y,   KC_U ,  KC_I ,   KC_O ,   KC_P ,   KC_BSLS,
-     KC_LCTL ,    KC_A ,  KC_S ,  KC_D  ,   LT(_NUMPAD,KC_F), KC_G,                                          KC_H,   KC_J ,  KC_K ,   KC_L ,   KC_SCLN, CTL_QUOT,
-     HYPR(KC_5),  KC_Z ,  KC_X ,  KC_C  ,   KC_V ,            KC_B , OSM(MOD_LSFT), OSM(MOD_LALT),     OSM(MOD_RALT)  , OSM(MOD_RSFT), KC_N,   KC_M ,  KC_COMM, KC_DOT,  KC_SLSH, MACLOCK,
-       j                        KC_LEFT , KC_RIGHT, KC_ESC, KC_LGUI, KC_BSPC,                    KC_ENT ,LT(_SYMBOLS, KC_SPC), KC_ESC, KC_UP, KC_DOWN,
-        KC_MUTE, KC_NO,  KC_NO, KC_NO, KC_NO,                                                                KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO),
+     KC_GRAVE,    KC_1 ,  KC_2   ,  KC_3  ,   KC_4 ,            KC_5 ,                                         KC_6 ,  KC_7 ,  KC_8 ,   KC_9 ,   KC_0 ,   KC_MINS,
+     KC_TAB  ,    KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,            KC_T ,                                         KC_Y,   KC_U ,  KC_I ,   KC_O ,   KC_P ,   KC_BSLS,
+     KC_LCTL ,    KC_A ,  KC_S   ,  KC_D  ,   LT(_NUMPAD,KC_F), KC_G,                                          KC_H,   KC_J ,  KC_K ,   KC_L ,   KC_SCLN, CTL_QUOT,
+     HYPR(KC_5),  KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,            KC_B , OSM(MOD_LSFT), OSM(MOD_LALT),     OSM(MOD_RALT)  , OSM(MOD_RSFT), KC_N,   KC_M ,  KC_COMM, KC_DOT,  KC_SLSH, MACLOCK,
+                                KC_LEFT , KC_RIGHT, KC_ESC, KC_LGUI, KC_BSPC,                    KC_ENT ,LT(_SYMBOLS, KC_SPC), KC_ESC, KC_UP, KC_DOWN, KC_MUTE, KC_NO,  KC_NO, KC_NO, KC_NO,                                                                KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO),
 
 
 
