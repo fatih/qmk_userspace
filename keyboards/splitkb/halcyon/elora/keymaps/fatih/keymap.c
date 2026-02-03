39b30f5cb1c42479d9f7a95adfa0c2ee55dda042
Fatih Arslan <ftharsln@gmail.com>
2026-02-03 22:32:19 +0300

    ƒix alt
    Signed-off-by: Fatih Arslan <ftharsln@gmail.com>

--- keyboards/splitkb/halcyon/elora/keymaps/fatih/keymap.c
+++ keyboards/splitkb/halcyon/elora/keymaps/fatih/keymap.c
@@ -52,8 +52,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_GRAVE,    KC_1 ,  KC_2   ,  KC_3  ,   KC_4 ,            KC_5 ,                                         KC_6 ,  KC_7 ,  KC_8 ,   KC_9 ,   KC_0 ,   KC_MINS,
      KC_TAB  ,    KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,            KC_T ,                                         KC_Y,   KC_U ,  KC_I ,   KC_O ,   KC_P ,   KC_BSLS,
      KC_LCTL ,    KC_A ,  KC_S   ,  KC_D  ,   LT(_NUMPAD,KC_F), KC_G ,                                         KC_H,   KC_J ,  KC_K ,   KC_L ,   KC_SCLN, CTL_QUOT,
-     HYPR(KC_5),  KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,            KC_B , OSM(MOD_LSFT), OSM(MOD_LALT),     KC_ESC, OSM(MOD_RALT)  , KC_N,   KC_M ,  KC_COMM, KC_DOT,  KC_SLSH, MACLOCK,
-                                KC_LEFT, KC_RIGHT , OSM(MOD_LSFT),  KC_LGUI, KC_BSPC,                    KC_ENT ,LT(_SYMBOLS, KC_SPC), OSM(MOD_RALT), KC_DOWN, KC_UP, KC_MUTE, KC_NO,  KC_NO, KC_NO, KC_NO,                                                                KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO),
+     HYPR(KC_5),  KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,            KC_B , OSM(MOD_LALT), KC_ESC,     KC_ESC, OSM(MOD_RALT)  , KC_N,   KC_M ,  KC_COMM, KC_DOT,  KC_SLSH, MACLOCK,
+                                KC_LEFT, KC_RIGHT , OSM(MOD_LSFT),  KC_LGUI, KC_BSPC,                    KC_ENT ,LT(_SYMBOLS, KC_SPC), OSM(MOD_RSFT), KC_DOWN, KC_UP, KC_MUTE, KC_NO,  KC_NO, KC_NO, KC_NO,                                                                KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO),
 
 
 
