e755b2b77e38060cba1f18e3e1806fa2f6159ada
Fatih Arslan <ftharsln@gmail.com>
2026-06-01 11:02:43 +0300

    Move shift to the side
    Signed-off-by: Fatih Arslan <ftharsln@gmail.com>

--- keyboards/splitkb/halcyon/elora/keymaps/fatih/keymap.c
+++ keyboards/splitkb/halcyon/elora/keymaps/fatih/keymap.c
@@ -39,9 +39,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
  * |  Ctrl  |   A  |   S  |   D  |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |Ctrl/' "|
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * | HYP 5  |   Z  |   X  |   C  |   V  |   B  | Shift| LAlt |  | RAlt | Shift|   N  |   M  | ,  < | . >  | /  ? |  Lock  |
+ * | LSHIFT |   Z  |   X  |   C  |   V  |   B  | Shift|HYP 5|  | Lock | ESC  |   N  |   M  | ,  < | . >  | /  ? | RSHIFT |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        | ESC  | Left | Right| GUI  | Bpsc |  | Enter| Space| Down | Up   | ESC  |
+ *                        | Lalt | Left | Right| GUI  | Bpsc |  | Enter| Space| Down | Up   | Ralt |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  * ,-----------------------------------.                                              ,-----------------------------------.
@@ -52,8 +52,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_GRAVE,    KC_1 ,  KC_2   ,  KC_3  ,   KC_4 ,            KC_5 ,                                         KC_6 ,  KC_7 ,  KC_8 ,   KC_9 ,   KC_0 ,   KC_MINS,
      KC_TAB  ,    KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,            KC_T ,                                         KC_Y,   KC_U ,  KC_I ,   KC_O ,   KC_P ,   KC_BSLS,
      KC_LCTL ,    KC_A ,  KC_S   ,  KC_D  ,   LT(_NUMPAD,KC_F), KC_G ,                                         KC_H,   KC_J ,  KC_K ,   KC_L ,   KC_SCLN, CTL_QUOT,
-     HYPR(KC_5),  KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,            KC_B , OSM(MOD_LSFT), OSM(MOD_LALT),     OSM(MOD_RALT)  , OSM(MOD_RSFT), KC_N,   KC_M ,  KC_COMM, KC_DOT,  KC_SLSH, MACLOCK,
-                                KC_ESC, KC_LEFT , KC_RIGHT,  KC_LGUI, KC_BSPC,                    KC_ENT ,LT(_SYMBOLS, KC_SPC), KC_DOWN, KC_UP, KC_ESC, KC_MUTE, KC_NO,  KC_NO, KC_NO, KC_NO,                                                                KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO),
+     OSM(MOD_LSFT),  KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,            KC_B , OSM(MOD_LSFT), HYPR(KC_5),     MACLOCK , KC_ESC, KC_N,   KC_M ,  KC_COMM, KC_DOT,  KC_SLSH, OSM(MOD_RSFT),
+                                OSM(MOD_ALT), KC_LEFT , KC_RIGHT,  KC_LGUI, KC_BSPC,                    KC_ENT ,LT(_SYMBOLS, KC_SPC), KC_DOWN, KC_UP, OSM(MOD_RALT), KC_MUTE, KC_NO,  KC_NO, KC_NO, KC_NO,                                                                KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO),
 
 
 
